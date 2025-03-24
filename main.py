e361b4ae709a9826280ba542439eab9e1fa23273
jaskatharina <jp.awesome0803@gmail.com>
2025-03-23 22:14:35 -0500

    draw and update players, asteroids, and shots

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
+from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,12 +16,40 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if(player.check_collisions(asteroid)):
+                print("Game over!")
+                return
+                
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+        
+        
+        dt = clock.tick(60) / 1000
+        
+        
 if __name__ == "__main__":
     main()
